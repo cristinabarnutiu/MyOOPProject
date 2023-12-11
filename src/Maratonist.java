3743a6c8b53f6185167765a38bb355721d8acaab
cristina <cristina.ilea2@yahoo.com>
2023-12-11 20:48:43 +0200

    second commit

---
+++ src/Maratonist.java
@@ -0,0 +1,10 @@
+public class Maratonist extends Sportiv{
+    @Override
+    void seAntreneaza() {
+        System.out.println("Maratonistul se antreneaza");
+    }
+
+    public void seOdihneste(){
+        System.out.println("Maratonistul se odihneste");
+    }
+}
